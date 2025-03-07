1c6fd250eed95a7f139d2857f0ebf451a3c30715
Charalampos Spanias <78966278+CSpanias@users.noreply.github.com>
2025-03-07 15:37:08 +0000

    updated

--- script.js
+++ script.js
@@ -72,6 +72,7 @@ for (var i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
+        // Check the current display state and toggle it
         if (content.style.display === "block") {
             content.style.display = "none";
         } else {
