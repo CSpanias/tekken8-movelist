b9a54e33e0f6da5849b2b24a30abaf55853d3f6b
Charalampos Spanias <78966278+CSpanias@users.noreply.github.com>
2025-03-07 15:34:48 +0000

    updated

--- script.js
+++ script.js
@@ -65,3 +65,17 @@ function filterMoves() {
     );
     displayMoves(filteredMoves, "azucena-body");
 }
+
+// JavaScript for collapsible functionality
+var coll = document.getElementsByClassName("collapsible");
+for (var i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
